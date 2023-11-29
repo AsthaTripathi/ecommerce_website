c8a2286f1be38fdb9d71c34b1d1edabe75f64188
Astha Tripathi <asthatripathi1012@gmail.com>
2023-11-29 12:51:14 +0530

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import DataProvider from './context/DataProvider';
 import DetailView from './components/details/DetailView';
 import Cart from './components/cart/Cart';
 
-
 import { Box } from '@mui/material';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -17,7 +16,7 @@ function App()  {
       <Header />
       <Box style = {{ marginTop:54 }}>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
       
       <Route path='/product/:id' element={<DetailView />} />
       <Route path='/cart' element={<Cart />} />
